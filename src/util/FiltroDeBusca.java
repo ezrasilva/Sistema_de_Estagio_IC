18e134fd6d171456e5b331432f3733a3389d3739
DaniloSovano <danilosovano@gmail.com>
2025-09-14 19:26:09 -0300

    Clean up comments in FiltroDeBusca class

--- src/util/FiltroDeBusca.java
+++ src/util/FiltroDeBusca.java
@@ -5,17 +5,14 @@ import java.util.stream.Collectors;
 import src.model.TipoVaga;
 import src.model.Vaga;
 
-// Classe com métodos estáticos para filtragem (requisito 3b)
 public class FiltroDeBusca {
 
-    // Método estático (requisito 3b)
     public static List<Vaga> filtrarPorTipo(List<Vaga> vagas, TipoVaga tipo) {
         return vagas.stream()
                 .filter(vaga -> vaga.getTipo().equals(tipo))
                 .collect(Collectors.toList());
     }
 
-    // Método estático (requisito 3b)
     public static List<Vaga> filtrarPorTitulo(List<Vaga> vagas, String palavraChave) {
         return vagas.stream()
                 .filter(vaga -> vaga.getTitulo().toLowerCase().contains(palavraChave.toLowerCase()))
